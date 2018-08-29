dc331533cb0d9d90be4bc74b4dc48fe3bab9dc8e
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-08-29 18:07:23 -0500

    Initial commit

---
+++ CIS2323_Week02_Quiz.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+public class CIS2323_Week02_Quiz{
+	public static void main(String[] Args){
+		JOptionPane.showMessageDialog(null, "Full name: James A Hall Jr\n" +
+		"\n" +
+		"This week I learned that JOptionPane can indeed\n" +
+		"do multiple lines of text. However if you don't\n" +
+		"use the 'new line' operator \\n, it will not\n" +
+		"do what you want it to do, and just explode." + 
+		"\n" +
+		"It is also cleaner if you use string concatenation\n" +
+		"(I think I spelled that right..(with spell check !))");
+	}
+}
